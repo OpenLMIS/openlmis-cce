a310dd5e8ecf79e9ac9cfc23fb9d2aeb4a511a04
Łukasz Lewczyński <llewczynski@soldevelo.com>
2017-10-17 11:26:25 +0200

    OLMIS-3211: Modified CatalogItemWriterTest (FEOLMIS-2013)

--- src/test/java/org/openlmis/cce/web/csv/recordhandler/CatalogItemWriterTest.java
+++ src/test/java/org/openlmis/cce/web/csv/recordhandler/CatalogItemWriterTest.java
@@ -17,34 +17,51 @@ package org.openlmis.cce.web.csv.recordhandler;
 
 import static java.util.Collections.singletonList;
 import static org.assertj.core.util.Lists.emptyList;
-import static org.assertj.core.util.Lists.newArrayList;
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.anyListOf;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import org.junit.Before;
+import org.assertj.core.util.Lists;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.openlmis.cce.domain.CatalogItem;
 import org.openlmis.cce.repository.CatalogItemRepository;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
-
+@RunWith(MockitoJUnitRunner.class)
 public class CatalogItemWriterTest {
+  private static final String EQUIPMENT_CODE_PREFIX = "equipmentCode";
+  private static final String MANUFACTURER_PREFIX = "manufacturer";
+  private static final String MODEL_PREFIX = "model";
+
+  private static final String EQUIPMENT_CODE_1 = EQUIPMENT_CODE_PREFIX + 1;
+  private static final String EQUIPMENT_CODE_2 = EQUIPMENT_CODE_PREFIX + 2;
+  private static final String EQUIPMENT_CODE_3 = EQUIPMENT_CODE_PREFIX + 3;
+
+  private static final String MANUFACTURER_1 = MANUFACTURER_PREFIX + 1;
+  private static final String MANUFACTURER_2 = MANUFACTURER_PREFIX + 2;
+  private static final String MANUFACTURER_3 = MANUFACTURER_PREFIX + 3;
+  private static final String MANUFACTURER_4 = MANUFACTURER_PREFIX + 4;
+
+  private static final String MODEL_1 = MODEL_PREFIX + 1;
+  private static final String MODEL_2 = MODEL_PREFIX + 2;
+  private static final String MODEL_3 = MODEL_PREFIX + 3;
+  private static final String MODEL_4 = MODEL_PREFIX + 4;
+
 
   @Captor
   private ArgumentCaptor<Iterable<CatalogItem>> catalogItemsCaptor;
-
-  private static final String EQCODE = "eqcode";
-  private static final String SOME_MAKE = "someManufacturer";
-  private static final String SOME_MODEL = "someModel";
 
   @Mock
   private CatalogItemRepository catalogItemRepository;
@@ -52,68 +69,104 @@ public class CatalogItemWriterTest {
   @InjectMocks
   private CatalogItemWriter catalogItemWriter;
 
-  private CatalogItem catalogItem;
-  private CatalogItem existingCatalogItem;
-  private List<CatalogItem> catalogItems;
-
-  @Before
-  public void setUp() {
-    MockitoAnnotations.initMocks(this);
-
-    catalogItem = new CatalogItem();
-    catalogItem.setEquipmentCode(EQCODE);
-    catalogItem.setManufacturer(SOME_MAKE);
-    catalogItem.setModel(SOME_MODEL);
-
-    existingCatalogItem = new CatalogItem();
-    existingCatalogItem.setId(UUID.randomUUID());
-    existingCatalogItem.setEquipmentCode(EQCODE);
-    existingCatalogItem.setManufacturer(SOME_MAKE);
-    existingCatalogItem.setModel(SOME_MODEL);
-
-    catalogItems = singletonList(catalogItem);
-  }
-
-  @Test
-  public void shouldSetIdIfExistingItemFoundByCode() {
-    when(catalogItemRepository.findExisting(anyListOf(CatalogItem.class)))
-        .thenReturn(singletonList(existingCatalogItem));
-
-    catalogItemWriter.write(catalogItems);
-
-    verify(catalogItemRepository).save(catalogItemsCaptor.capture());
-    assertEquals(
-        existingCatalogItem.getId(),
-        newArrayList(catalogItemsCaptor.getValue()).get(0).getId()
-    );
-  }
-
-  @Test
-  public void shouldSetIdIfExistingItemFoundByTypeAndModel() {
-    catalogItem.setEquipmentCode(null);
-    catalogItem.setManufacturer(SOME_MAKE);
-    catalogItem.setModel(SOME_MODEL);
-
-    when(catalogItemRepository.findExisting(anyListOf(CatalogItem.class)))
-        .thenReturn(singletonList(existingCatalogItem));
-
-    catalogItemWriter.write(catalogItems);
-
-    verify(catalogItemRepository).save(catalogItemsCaptor.capture());
-    assertEquals(
-        existingCatalogItem.getId(),
-        newArrayList(catalogItemsCaptor.getValue()).get(0).getId()
-    );
-  }
-
   @Test
   public void shouldNotSetIdIfExistingItemNotFound() {
+    //given
+    CatalogItem toSave = create(EQUIPMENT_CODE_1, MANUFACTURER_1, MODEL_1);
+    List<CatalogItem> toSaveList = singletonList(toSave);
+
+    // when
     when(catalogItemRepository.findExisting(anyListOf(CatalogItem.class)))
         .thenReturn(emptyList());
 
-    catalogItemWriter.write(catalogItems);
+    catalogItemWriter.write(toSaveList);
 
-    verify(catalogItemRepository).save(catalogItems);
+    // then
+    verify(catalogItemRepository).save(toSaveList);
+  }
+
+  @Test
+  public void shouldFindByEquipmentCode() throws Exception {
+    // given
+    CatalogItem toSave = create(EQUIPMENT_CODE_1, MANUFACTURER_1, MODEL_1);
+    List<CatalogItem> toSaveList = singletonList(toSave);
+
+    CatalogItem db1 = create(EQUIPMENT_CODE_1, MANUFACTURER_1, MODEL_1);
+    CatalogItem db2 = create(EQUIPMENT_CODE_2, MANUFACTURER_2, MODEL_2);
+    CatalogItem db3 = create(null, MANUFACTURER_3, MODEL_3);
+    CatalogItem db4 = create(null, MANUFACTURER_4, MODEL_4);
+    List<CatalogItem> fromDb = Arrays.asList(db1, db2, db3, db4);
+
+    // when
+    when(catalogItemRepository.findExisting(anyListOf(CatalogItem.class)))
+        .thenReturn(fromDb);
+
+    catalogItemWriter.write(toSaveList);
+
+    //then
+    verify(catalogItemRepository).save(catalogItemsCaptor.capture());
+
+    List<CatalogItem> captured = Lists.newArrayList(catalogItemsCaptor.getValue());
+    assertThat(captured, hasSize(1));
+    assertThat(captured.get(0).getId(), equalTo(db1.getId()));
+  }
+
+  @Test
+  public void shouldFindByManufacturerAndModel() throws Exception {
+    // given
+    CatalogItem toSave = create(null, MANUFACTURER_3, MODEL_3);
+    List<CatalogItem> toSaveList = singletonList(toSave);
+
+    CatalogItem db1 = create(EQUIPMENT_CODE_1, MANUFACTURER_1, MODEL_1);
+    CatalogItem db2 = create(EQUIPMENT_CODE_2, MANUFACTURER_2, MODEL_2);
+    CatalogItem db3 = create(null, MANUFACTURER_3, MODEL_3);
+    CatalogItem db4 = create(null, MANUFACTURER_4, MODEL_4);
+    List<CatalogItem> fromDb = Arrays.asList(db1, db2, db3, db4);
+
+    // when
+    when(catalogItemRepository.findExisting(anyListOf(CatalogItem.class)))
+        .thenReturn(fromDb);
+
+    catalogItemWriter.write(toSaveList);
+
+    //then
+    verify(catalogItemRepository).save(catalogItemsCaptor.capture());
+
+    List<CatalogItem> captured = Lists.newArrayList(catalogItemsCaptor.getValue());
+    assertThat(captured, hasSize(1));
+    assertThat(captured.get(0).getId(), equalTo(db3.getId()));
+  }
+
+  @Test
+  public void shouldNotFindIfEquipmentCodeNotMatch() throws Exception {
+    // given
+    CatalogItem toSave = create(EQUIPMENT_CODE_3, MANUFACTURER_3, MODEL_3);
+    List<CatalogItem> toSaveList = singletonList(toSave);
+
+    CatalogItem db1 = create(EQUIPMENT_CODE_1, MANUFACTURER_1, MODEL_1);
+    CatalogItem db2 = create(EQUIPMENT_CODE_2, MANUFACTURER_2, MODEL_2);
+    CatalogItem db3 = create(null, MANUFACTURER_3, MODEL_3);
+    CatalogItem db4 = create(null, MANUFACTURER_4, MODEL_4);
+    List<CatalogItem> fromDb = Arrays.asList(db1, db2, db3, db4);
+
+    // when
+    when(catalogItemRepository.findExisting(anyListOf(CatalogItem.class)))
+        .thenReturn(fromDb);
+
+    catalogItemWriter.write(toSaveList);
+
+    //then
+    verify(catalogItemRepository).save(toSaveList);
+  }
+
+  private CatalogItem create(String equipmentCode, String manufacturer, String model) {
+    CatalogItem item = new CatalogItem();
+    item.setId(UUID.randomUUID());
+    item.setEquipmentCode(equipmentCode);
+    item.setManufacturer(manufacturer);
+    item.setModel(model);
+
+    return item;
   }
 
 }
