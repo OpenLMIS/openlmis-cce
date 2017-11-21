cb964991a2b8a44135cad073dca525437e7ae657
Łukasz Lewczyński <llewczynski@soldevelo.com>
2017-11-21 10:19:06 +0100

    OLMIS-3405: Added warm up before perf tests

--- performance/test.sh
+++ performance/test.sh
@@ -4,18 +4,30 @@
 # remember to quote the URL, so for example:
 # ./test.sh 'http://localhost'
 
-# determine base uri from first argument or use default
 URL='https://test.openlmis.org'
+
+run_tests() {
+    export BASE_URL="${URL}"
+    export DURATION=$1
+
+    docker run --rm -e BASE_URL -v $(pwd):/bzt-configs \
+      -v $(pwd)/../build/performance-artifacts:/tmp/artifacts \
+      undera/taurus \
+      -o modules.jmeter.properties.base-uri="${BASE_URL}" \
+      -o modules.jmeter.properties.scenario-duration="${DURATION}" \
+      -o reporting.2.dump-xml=/tmp/artifacts/stats.xml \
+      config.yml \
+      tests/*.yml
+}
+
+# determine base uri from first argument or use default
 if [ ! -z ${1+x} ]; then
   URL=$1
 fi
 
 echo "Running performance tests against: $URL"
-export BASE_URL="${URL}"
-docker run --rm -e BASE_URL -v $(pwd):/bzt-configs \
-  -v $(pwd)/../build/performance-artifacts:/tmp/artifacts \
-  undera/taurus \
-  -o modules.jmeter.properties.base-uri="${BASE_URL}" \
-  -o reporting.2.dump-xml=/tmp/artifacts/stats.xml \
-  config.yml \
-  tests/*.yml
+
+echo "Warm up (30s)"
+run_tests 30
+echo "Tests (150s)"
+run_tests 150
