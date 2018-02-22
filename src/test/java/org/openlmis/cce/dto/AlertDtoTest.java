d0df5f711704e71eee895ee1cb6eef829121da12
Chongsun Ahn <cahn922@yahoo.com>
2018-02-21 23:01:18 -0800

    OLMIS-4105 Add test coverage
    For Sonar.

---
+++ src/test/java/org/openlmis/cce/dto/AlertDtoTest.java
@@ -0,0 +1,80 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+package org.openlmis.cce.dto;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import nl.jqno.equalsverifier.EqualsVerifier;
+import nl.jqno.equalsverifier.Warning;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AlertDtoTest {
+
+  private AlertDto alertDto;
+  
+  @Before
+  public void setUp() {
+    alertDto = new AlertDto();
+  }
+  
+  @Test
+  public void equalsContract() {
+    EqualsVerifier.forClass(AlertDto.class)
+        // AlertDto cannot be final because then we cannot mock it
+        .suppress(Warning.STRICT_INHERITANCE)
+        // AlertDto fields cannot be final
+        .suppress(Warning.NONFINAL_FIELDS)
+        .withIgnoredFields("inventoryItemRepository")
+        .verify();
+  }
+
+  @Test
+  public void setEndTimestampShouldCopyToEndTsIfNotNull() {
+    //given
+    ZonedDateTime zdtNow = ZonedDateTime.now();
+
+    //when
+    alertDto.setEndTimestamp(zdtNow);
+
+    //then
+    assertEquals(zdtNow.toInstant().toEpochMilli(), (long)alertDto.getEndTs());
+  }
+
+  @Test
+  public void getEndTimestampShouldCopyFromEndTsIfNotNull() {
+    //given
+    long epochMilli = 1514793600000L;
+    alertDto.setEndTs(epochMilli);
+    
+    //then
+    assertEquals(ZonedDateTime.ofInstant(Instant.ofEpochMilli(epochMilli), ZoneOffset.UTC),
+        alertDto.getEndTimestamp());
+  }
+
+  @Test
+  public void getInventoryItemShouldReturnNullIfRepositoryIsNull() {
+    //given
+    alertDto.setInventoryItemRepository(null);
+
+    //then
+    assertNull(alertDto.getInventoryItem());
+  }
+}
